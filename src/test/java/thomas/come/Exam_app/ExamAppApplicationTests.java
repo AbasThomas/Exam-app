a3b613979e327bac64a97cd5cc44d2f3350e49a3
Thomas Abas <abasienyenethomas2@gmail.com>
2026-01-20 13:40:16 +0100

    first commit

---
+++ src/test/java/thomas/come/Exam_app/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package thomas.come.Exam_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
